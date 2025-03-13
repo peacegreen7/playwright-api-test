bbc5cd61373d7a4c3c1840065c53c53fcbd574fa
yenhtt <thanhyen107@gmail.com>
2025-03-13 10:32:19 +0700

    implement simple mocking api

---
+++ tests/api.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+import tags from '../test-data/tags.json';
+
+test.beforeEach(async ({ page }) => {
+  await page.route('https://conduit-api.bondaracademy.com/api/tags', async route => {
+    await route.fulfill({
+      body: JSON.stringify(tags)
+    })
+  })
+
+  await page.goto('https://conduit.bondaracademy.com/')
+})
+
+test('has title', async ({ page }) => {
+  await expect(page.locator('.navbar-brand')).toHaveText('conduit')
+})
