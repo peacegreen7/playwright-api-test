8d46f6077100b422eacc197e2480061fa1ea3f79
yenhtt <thanhyen107@gmail.com>
2025-03-20 06:08:41 +0700

    optimize class

--- learn-ts/lesson3_counter_app.ts
+++ learn-ts/lesson3_counter_app.ts
@@ -38,30 +38,41 @@
 
 // convert to basic class
 class Counter {
-    counter: number;
+    private counter: number;
 
     constructor(counter: number) {
+        if (counter < 0) {
+            throw new Error('Counter cannot be initialized with a negative value');
+        }
         this.counter = counter;
     }
 
-    increaseCounter(): number {
-        return this.counter++;
-    }
-
-    decreaseCounter(): any {
-        if (this.counter > 0)
-            return this.counter--;
-        else
-            console.log('Counter cannot less than 0')
-    }
-
-    resetCounter(): number {
-        this.counter = 0;
+    getCounter(): number {
         return this.counter;
+    }
+
+    increaseCounter(): void {
+        this.counter++;
+        console.log(`counter increase to: ${this.getCounter()}`);
+    }
+
+    decreaseCounter(): void {
+        if (this.counter > 0) {
+            this.counter--;
+            console.log(`Counter decrease to: ${this.getCounter()}`)
+        }
+        else
+            console.log('Counter cannot be less than 0')
+    }
+
+    resetCounter(): void {
+        this.counter = 0;
+        console.log(`Counter reset to 0`);
     }
 }
 
 let counter1 = new Counter(5);
+console.log(`Initial counter: ${counter1.getCounter()}`)
 counter1.increaseCounter();
 counter1.increaseCounter();
 counter1.increaseCounter();
@@ -72,4 +83,4 @@ counter1.increaseCounter();
 counter1.resetCounter();
 counter1.decreaseCounter();
 counter1.increaseCounter();
-console.log(counter1);
+console.log(`Final counter: ${counter1.getCounter()}`);
