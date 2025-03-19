b8193712f6bae497571f2582c65d2a06af047a51
yenhtt <thanhyen107@gmail.com>
2025-03-20 05:59:18 +0700

    create simple counter class

--- learn-ts/lesson3_counter_app.ts
+++ learn-ts/lesson3_counter_app.ts
@@ -6,32 +6,70 @@
  * Reset the counter to zero.
  */
 
-var counter = 0;
+// var counter = 0;
 
-function increaseCounter() {
-    counter++;
-    console.log('Counter: ' + counter);
-}
+// function increaseCounter() {
+//     counter++;
+//     console.log('Counter: ' + counter);
+// }
 
-function decreaseCounter() {
-    if (counter > 0) {
-        counter--;
-        console.log('Counter: ' + counter);
-    } else {
-        console.log('Counter cannot be less than 0');
+// function decreaseCounter() {
+//     if (counter > 0) {
+//         counter--;
+//         console.log('Counter: ' + counter);
+//     } else {
+//         console.log('Counter cannot be less than 0');
+//     }
+// }
+
+// function resetCounter() {
+//     counter = 0;
+//     console.log('Counter: ' + counter);
+// }
+
+// function testCounterApp() {
+//     increaseCounter();
+//     increaseCounter();
+//     resetCounter();
+//     decreaseCounter();
+//     resetCounter();
+// }
+// testCounterApp();
+
+// convert to basic class
+class Counter {
+    counter: number;
+
+    constructor(counter: number) {
+        this.counter = counter;
+    }
+
+    increaseCounter(): number {
+        return this.counter++;
+    }
+
+    decreaseCounter(): any {
+        if (this.counter > 0)
+            return this.counter--;
+        else
+            console.log('Counter cannot less than 0')
+    }
+
+    resetCounter(): number {
+        this.counter = 0;
+        return this.counter;
     }
 }
 
-function resetCounter() {
-    counter = 0;
-    console.log('Counter: ' + counter);
-}
-
-function testCounterApp() {
-    increaseCounter();
-    increaseCounter();
-    resetCounter();
-    decreaseCounter();
-    resetCounter();
-}
-testCounterApp();
+let counter1 = new Counter(5);
+counter1.increaseCounter();
+counter1.increaseCounter();
+counter1.increaseCounter();
+counter1.decreaseCounter();
+counter1.decreaseCounter();
+counter1.decreaseCounter();
+counter1.increaseCounter();
+counter1.resetCounter();
+counter1.decreaseCounter();
+counter1.increaseCounter();
+console.log(counter1);
